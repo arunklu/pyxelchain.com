0b7109bcb160ffe4cf831f4c59654cb9c3280606
sanjipun <iamsanjipun@gmail.com>
2022-08-18 22:39:09 +0545

    CU-2q41bx5 - Flash of Unstyled Text and Navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,4 @@
-
-import '@fontsource/inter/400.css'
-import '@fontsource/inter/500.css'
-import '@fontsource/inter/600.css'
-import '@fontsource/inter/700.css'
-
+import '@fontsource/inter/'
 import '@fontsource/iosevka'
 
 import 'tippy.js/dist/tippy.css'
