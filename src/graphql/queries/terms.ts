052d53bd5302b583bae31dbc3891e9ad72bd6fd1
sanjipun <iamsanjipun@gmail.com>
2022-08-24 21:38:04 +0545

    CU-2ubv66k: dad terms graphql query

---
+++ src/graphql/queries/terms.ts
@@ -0,0 +1,16 @@
+import { gql } from '@apollo/client'
+
+export const GET_TERMS_PRIVACY_COOKIE_QUERY = gql`
+  query GetTermsPrivacyCookieQuery($slug: String) {
+    privacyTermsCookies(filters: { slug: { eq: $slug } }) {
+      data {
+        attributes {
+          header
+          publishedAt
+          description
+          pdfLink
+        }
+      }
+    }
+  }
+`
