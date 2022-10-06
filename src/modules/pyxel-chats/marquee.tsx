477eb12ca9ca7f0698a7be34972989f0f7aae3f0
sanjipun <iamsanjipun@gmail.com>
2022-10-06 21:26:21 +0545

    CU-344j4fr: update marquee image links

--- src/modules/pyxel-chats/marquee.tsx
+++ src/modules/pyxel-chats/marquee.tsx
@@ -6,13 +6,13 @@ const PCMarquee: React.FC = () => {
   return (
     <Box mt="140px">
       <Marquee gradientColor={[2, 6, 21]} speed={100}>
-        <Image mx="15px" src="/podcast/listen-notes.svg" />
-        <Image mx="15px" src="/podcast/google-podcasts.svg" />
+        <Image mx="15px" src="/podcast/listen.svg" />
+        <Image mx="15px" src="/podcast/google.svg" />
         <Image mx="15px" src="/podcast/stitcher.svg" />
         <Image mx="15px" src="/podcast/youtube.svg" />
-        <Image mx="15px" src="/podcast/amazon-music.svg" />
+        <Image mx="15px" src="/podcast/amazon.svg" />
         <Image mx="15px" src="/podcast/spotify.svg" />
-        <Image mx="15px" src="/podcast/apple-podcast.svg" />
+        <Image mx="15px" src="/podcast/apple.svg" />
       </Marquee>
     </Box>
   )
