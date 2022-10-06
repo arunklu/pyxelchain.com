e758ea88888543678b4c2bc694170c809c759829
sanjipun <iamsanjipun@gmail.com>
2022-10-06 21:25:57 +0545

    CU-344j4fr: update chats-list, strapi integration

--- src/modules/pyxel-chats/chats-list.tsx
+++ src/modules/pyxel-chats/chats-list.tsx
@@ -1,16 +1,30 @@
 import { Container, Flex, Grid, GridItem } from '@chakra-ui/react'
 import useHorizontalScroll from '@hooks/use-horizontal-scroll'
 import Tag from '@modules/articles/tag'
+import { PodcastEntity, TagEntity } from 'types/index'
 import React from 'react'
 import ChatsCard from './chats-card'
 
-interface ChatsListProps {}
+interface ChatsListProps {
+  podcasts: NonNullable<PodcastEntity>[]
+  tags: NonNullable<TagEntity>[]
+}
 
-const ChatsList: React.FC<ChatsListProps> = () => {
+const ChatsList: React.FC<ChatsListProps> = ({ podcasts, tags }) => {
   const { elRef } = useHorizontalScroll()
   const [selectedTag, setSelectedTag] = React.useState<string>('All')
 
-  const ARTICLES_TAGS = [{ tagName: 'All' }, { tagName: 'CryptoCurrency' }]
+  const PODCASTS_TAGS = [
+    { tagName: 'All' },
+    ...tags.map((tag) => {
+      return { tagName: tag?.attributes?.tagName }
+    }),
+  ]
+  const filteredPodcasts = podcasts
+    .filter((element) => element?.attributes?.podcast_tags?.data.some((tag) => tag.attributes?.tagName === selectedTag))
+    .map((element) => element)
+  const podcastData = selectedTag === 'All' ? podcasts : filteredPodcasts
+
   return (
     <Container maxWidth="container.2xl" px={{ base: '30px', lg: '50px', xl: '96px' }}>
       <Flex
@@ -34,24 +48,22 @@ const ChatsList: React.FC<ChatsListProps> = () => {
           },
         }}
       >
-        {ARTICLES_TAGS.map((tag, i) => (
+        {PODCASTS_TAGS?.map((tag, i) => (
           <Tag
             selectedTag={selectedTag}
             onClick={(e: string) => setSelectedTag(e)}
-            key={tag.tagName}
-            tagName={tag.tagName}
-            isLast={ARTICLES_TAGS.length === i + 1}
+            key={tag?.tagName}
+            tagName={tag?.tagName as string}
+            isLast={PODCASTS_TAGS?.length === i + 1}
           />
         ))}
       </Flex>
       <Grid gap="30px" mt="52px" templateColumns={{ base: 'repeat(1,1fr)', lg: 'repeat(2,1fr)', xl: 'repeat(3,1fr)' }}>
-        {Array(10)
-          .fill('yo')
-          .map((chat, i: number) => (
-            <GridItem h="602px" key={i}>
-              <ChatsCard hoverable={true} chat={chat} />
-            </GridItem>
-          ))}
+        {podcastData?.map((chat, i: number) => (
+          <GridItem h="602px" key={i}>
+            <ChatsCard hoverable={true} chat={chat} />
+          </GridItem>
+        ))}
       </Grid>
     </Container>
   )
