2c6b4e7aad3397f260e69dc6b01d60c919c1b606
sanjipun <iamsanjipun@gmail.com>
2022-10-11 20:36:00 +0545

    CU-344j4fr: add seo to pyxelchats page

--- src/pages/pyxel-chats/index.tsx
+++ src/pages/pyxel-chats/index.tsx
@@ -8,6 +8,7 @@ import axios, { AxiosResponse } from 'axios'
 import { print } from 'graphql'
 import { GetStaticProps } from 'next'
 import React from 'react'
+import { SEO } from '@components/seo'
 
 interface IndexProps {
   data: {
@@ -22,6 +23,7 @@ const Index: React.FC<IndexProps> = ({ data }) => {
 
   return (
     <>
+      <SEO title={copy?.attributes?.seo?.metatitle} description={copy?.attributes?.seo?.metadescription} />
       <PyxelChats copy={copy?.attributes} />
       <PCMarquee />
       <Divider w="full" color="#C9D2D8" mt="59px" opacity={0.1} />
