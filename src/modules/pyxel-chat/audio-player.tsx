f6d0d8370640f08074596c690def6d4d08a67000
sanjipun <iamsanjipun@gmail.com>
2022-10-12 11:11:32 +0545

    CU-344j4fr: Update audio player ui

--- src/modules/pyxel-chat/audio-player.tsx
+++ src/modules/pyxel-chat/audio-player.tsx
@@ -1,11 +1,19 @@
-import { Box, Button, Slider, SliderTrack, SliderFilledTrack, SliderThumb } from '@chakra-ui/react'
+import { Box, Slider, SliderTrack, SliderFilledTrack, SliderThumb, Flex, HStack, Image } from '@chakra-ui/react'
+import { FaPlayCircle } from '@react-icons/all-files/fa/FaPlayCircle'
+import { FaPauseCircle } from '@react-icons/all-files/fa/FaPauseCircle'
+import { MdVolumeUp } from '@react-icons/all-files/md/MdVolumeUp'
+import { MdVolumeOff } from '@react-icons/all-files/md/MdVolumeOff'
+import { AiFillStepBackward } from '@react-icons/all-files/ai/AiFillStepBackward'
+import { AiFillStepForward } from '@react-icons/all-files/ai/AiFillStepForward'
+import { Text } from '@components/typography'
 import React, { useRef } from 'react'
 
 interface AudioPlayerProps {
   audioLink: string
+  title: string
 }
 
-const AudioPlayer: React.FC<AudioPlayerProps> = ({ audioLink }) => {
+const AudioPlayer: React.FC<AudioPlayerProps> = ({ audioLink, title }) => {
   const audioRef = useRef<HTMLAudioElement>(null)
   const [curTime, setCurTime] = React.useState<number>(0)
   const [duration, setDuration] = React.useState<number>(0.001)
@@ -62,27 +70,51 @@ const AudioPlayer: React.FC<AudioPlayerProps> = ({ audioLink }) => {
     setCurPercentage(e)
     audioRef!.current!.currentTime = (duration / 100) * e
   }
-
+  const toTime = (seconds: number) => {
+    const date = new Date(0)
+    date.setSeconds(seconds)
+    return date.toISOString().substr(11, 8)
+  }
   return (
-    <Box>
+    <Flex gap={3} flexDir="column" justifyContent="space-between" alignItems="" w="full">
       <audio ref={audioRef}>
         <source src={audioLink} type="audio/ogg" />
         <source src={audioLink} type="audio/mpeg" />
         Your browser does not support the audio element.
       </audio>
-      <Button onClick={play}>Play</Button>
-      <Button onClick={pause}>Pause</Button>
-      <Button onClick={forward}>Forward</Button>
-      <Button onClick={backward}>backward</Button>
-      <Button onClick={mute}>mute</Button>
-      {Math.floor(curTime)}/{Math.floor(duration)}
+      <Text mt="9px" fontWeight={500} color="white">
+        {title}
+      </Text>
+      <Text fontWeight={500} color="white">
+        {toTime(curTime)} / {toTime(duration)}
+      </Text>
       <Slider colorScheme="audio" onChange={handleDrag} aria-label="slider-ex-1" value={curPercentage}>
         <SliderTrack>
           <SliderFilledTrack />
         </SliderTrack>
         <SliderThumb bg="#5CD1B4" />
       </Slider>
-    </Box>
+      <Flex mt="10px" mb="27px">
+        <Box cursor="pointer" mt="30px" onClick={mute}>
+          {!audioRef?.current?.muted && <MdVolumeUp fontSize={28} />}
+          {audioRef?.current?.muted && <MdVolumeOff fontSize={28} />}
+        </Box>
+        <HStack gap="2px" justifyContent="center" alignItems="center" w="full">
+          <HStack cursor="pointer" gap="2px" onClick={backward}>
+            <AiFillStepBackward fontSize={37} />
+            <Image src="/podcast/backward.svg" />
+          </HStack>
+          <HStack cursor="pointer" alignItems="center" justifyContent="center">
+            {audioRef?.current?.paused && <FaPlayCircle onClick={play} fontSize={50} />}
+            {!audioRef?.current?.paused && <FaPauseCircle onClick={pause} fontSize={50} />}
+          </HStack>
+          <HStack cursor="pointer" gap="2px" onClick={forward}>
+            <Image src="/podcast/forward.svg" />
+            <AiFillStepForward fontSize={37} />
+          </HStack>
+        </HStack>
+      </Flex>
+    </Flex>
   )
 }
 
