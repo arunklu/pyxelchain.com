fed4eb565419287f98bdb1cd0c33a83b4c738cd5
sanjipun <iamsanjipun@gmail.com>
2022-10-13 11:31:14 +0545

    CU-34w3zka: animate pyxis section

--- src/modules/home/pyxis/pyxis.tsx
+++ src/modules/home/pyxis/pyxis.tsx
@@ -6,18 +6,30 @@ import PyxButton from './pyx-button'
 import PyxCard from './pyx-card'
 import MarkdownRenderer, { HeadingRenderer } from '@components/markdown-renderer'
 import { useStrapiContextValue } from '@context/strapi-context'
+import { useInView } from 'framer-motion'
+import { ChakraBox } from 'theme/chakra-box'
 
 const Pyxis: React.FC = () => {
   const { getCopyBySectionId, getFeatureBySectionId } = useStrapiContextValue()
   const appDownloadCopy = getCopyBySectionId('home-app-download')
 
   const pyxisFeatures = getFeatureBySectionId('home-app-download')
-
+  const ref = React.useRef(null)
+  const isInView = useInView(ref, { once: true })
+  const ref2 = React.useRef(null)
+  const isInView2 = useInView(ref2, { once: true })
+  const ref3 = React.useRef(null)
+  const isInView3 = useInView(ref3, { once: true })
   return (
     <VStack mb="183px" mx="auto">
-      <Box maxW="894px">
+      <ChakraBox ref={ref} animate={{ y: [40, isInView ? 0 : 40] }} maxW="894px">
         <Box textAlign="center">
-          <HeadingRenderer title={appDownloadCopy?.title} titleWithGradient={appDownloadCopy?.titleWithGradient} />
+          <HeadingRenderer
+            center
+            mobilecenter
+            title={appDownloadCopy?.title}
+            titleWithGradient={appDownloadCopy?.titleWithGradient}
+          />
         </Box>
         <Box textAlign="center" maxW="720px" mt="20px" mx="auto">
           <MarkdownRenderer markdown={appDownloadCopy?.description} />
@@ -28,15 +40,32 @@ const Pyxis: React.FC = () => {
             Apple Store
           </PyxButton>
         </Flex>
-      </Box>
-      <Image display={{ base: 'block', sm: 'none' }} src="/svg/pyx-wallet/pyx-2.svg" alt="pyxis wallet image" />
-      <Image
-        display={{ base: 'none', sm: 'block' }}
-        src="/svg/pyx-wallet/pyxis-wallet-image.svg"
-        alt="pyxis wallet image"
-      />
+      </ChakraBox>
+      <ChakraBox ref={ref2} animate={{ y: [40, isInView2 ? 0 : 40] }}>
+        <Image display={{ base: 'block', sm: 'none' }} src="/svg/pyx-wallet/pyx-2.svg" alt="pyxis wallet image" />
+        <Image
+          display={{ base: 'none', sm: 'block' }}
+          src="/svg/pyx-wallet/pyxis-wallet-image.svg"
+          alt="pyxis wallet image"
+        />
+      </ChakraBox>
       <TwoByTwoGrid>
-        {pyxisFeatures.map((feature, i) => (feature ? <PyxCard key={i} data={feature} /> : null))}
+        {pyxisFeatures.map((feature, i) =>
+          feature ? (
+            <ChakraBox
+              key={i + 1}
+              ref={ref3}
+              // @ts-ignore no problem in operation, although type error appears.
+              transition={{
+                delay: (i + 1) % 2 === 1 ? 0.15 : 0.3,
+                ease: 'easeInOut',
+              }}
+              animate={{ y: [40, isInView3 ? 0 : 40] }}
+            >
+              <PyxCard data={feature} />
+            </ChakraBox>
+          ) : null
+        )}
       </TwoByTwoGrid>
     </VStack>
   )
