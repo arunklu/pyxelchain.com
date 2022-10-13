8f5aa956366c5b5378658f481f7d5c8a3b409b05
sanjipun <iamsanjipun@gmail.com>
2022-10-13 11:31:42 +0545

    CU-34w3zka: animate roadmap section

--- src/modules/home/roadmap/roadmap.tsx
+++ src/modules/home/roadmap/roadmap.tsx
@@ -1,18 +1,32 @@
 import React from 'react'
-import { Flex, Box, Image, SimpleGrid } from '@chakra-ui/react'
+import { Box, Image, SimpleGrid } from '@chakra-ui/react'
 
 import RoadmapCard from './roadmap-card'
 import { FIRST_HALF } from '@constants/roadmap-items'
 import Rocket from './rocket'
 import { HeadingRenderer } from '@components/markdown-renderer'
 import { useStrapiContextValue } from '@context/strapi-context'
+import { ChakraBox } from 'theme/chakra-box'
 
 const Roadmap = () => {
   const { getCopyBySectionId } = useStrapiContextValue()
   const roadmapCopy = getCopyBySectionId('home-roadmap')
   return (
     <Box mb={{ base: 20, md: 44 }} style={{ userSelect: 'none' }}>
-      <Flex mb={{ base: 7, md: 16 }} alignItems="center">
+      <ChakraBox
+        viewport={{ once: true }}
+        initial={{ y: 40 }}
+        whileInView={{
+          y: 0,
+          transition: {
+            duration: 1,
+            ease: 'easeInOut',
+          },
+        }}
+        display="flex"
+        mb={{ base: 7, md: 16 }}
+        alignItems="center"
+      >
         <Box mr={2}>
           <Image src="/svg/title-roadmap-pattern.svg" />
         </Box>
@@ -20,15 +34,28 @@ const Roadmap = () => {
           <HeadingRenderer title={roadmapCopy?.title} titleWithGradient={roadmapCopy?.titleWithGradient} />
           (Coming Soon)
         </Box>
-      </Flex>
-      <Box position="relative" filter="auto" blur="3px">
+      </ChakraBox>
+      <ChakraBox
+        viewport={{ once: true }}
+        initial={{ y: 40 }}
+        whileInView={{
+          y: 0,
+          transition: {
+            duration: 1,
+            ease: 'easeInOut',
+          },
+        }}
+        position="relative"
+        filter="auto"
+        blur="3px"
+      >
         <SimpleGrid columns={{ base: 1, md: 2 }} spacing={{ base: 5, md: 20 }} mb={{ base: 5, lg: 36 }}>
           {FIRST_HALF.map((roadmap) => (
             <RoadmapCard key={roadmap.quarter} roadmap={roadmap} />
           ))}
         </SimpleGrid>
         <Rocket />
-      </Box>
+      </ChakraBox>
     </Box>
   )
 }
