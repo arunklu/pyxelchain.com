93027248e6b9fa0fe49ea174fcdb6d83c78df591
Ralph Largo <devlargs@gmail.com>
2023-03-31 14:16:48 +0800

    remove gray background for contact us button

--- src/components/newsletter-form.tsx
+++ src/components/newsletter-form.tsx
@@ -1,5 +1,5 @@
-import { Flex, FormControl, Input, Text } from '@chakra-ui/react'
-import Link from './link';
+import { Box, FormControl, Input, Text } from '@chakra-ui/react'
+import Link from './link'
 import useDuplicateEmail from '@hooks/use-duplicate-email'
 import { showToast } from '@utils/toast-utils'
 import { useRouter } from 'next/router'
@@ -9,29 +9,23 @@ import Button from './button'
 type NewsletterFormTypes = {
   autoAlign?: boolean
 }
-const NewsletterForm: React.FC<NewsletterFormTypes> = ({ autoAlign }) => {
-
-  return (
-    <>
-      <Flex
-        mt={autoAlign ? '48px' : 0}
-        direction={{ base: 'column', lg: 'row' }}
-        justifyContent="space-between"
-        mx={{ base: 'auto', lg: autoAlign ? 'auto' : 0 }}
-        maxW="600px"
-        bg="rgba(255, 255, 255, 0.07)"
-        p={2}
-        rounded="lg"
-      >
-        <Link href="/services" display="flex" flexShrink={0} mb={8}>
-          <Button>Contact Our Team</Button>
-        </Link>
-      </Flex>
-      <Text color="white" mt="53px">
-        We don't spam, sell, or shill!
-      </Text>
-    </>
-  )
-}
-
+const NewsletterForm: React.FC<NewsletterFormTypes> = ({ autoAlign }) => (
+  <>
+    <Box
+      mt={autoAlign ? '48px' : 0}
+      justifyContent="space-between"
+      mx={{ base: 'auto', lg: autoAlign ? 'auto' : 0 }}
+      maxW="600px"
+      p={2}
+      rounded="lg"
+    >
+      <Link href="/services" mb={8}>
+        <Button>Contact Our Team</Button>
+      </Link>
+    </Box>
+    <Text color="white" mt="35px">
+      We don't spam, sell, or shill!
+    </Text>
+  </>
+)
 export default NewsletterForm
