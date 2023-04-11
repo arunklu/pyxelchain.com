df7018ca3de0a42b4dc937c8c819f99f76f5c58b
Ralph Largo <devlargs@gmail.com>
2023-04-11 21:40:15 +0800

    feat: update mobile layout

--- src/modules/home/upcoming-products.tsx
+++ src/modules/home/upcoming-products.tsx
@@ -25,21 +25,58 @@ const UpcomingProducts = () => {
           <BorderBox hoverable key={idx + 1} mt="30px">
             <Box h="full" bg="#020615">
               <Box h="full" px="15px" py="15px" bg="#020615">
-                <Flex h="280px" gap="32px" alignItems="center">
+                <Flex
+                  h={{
+                    base: 'full',
+                    md: '280px',
+                  }}
+                  gap="32px"
+                  alignItems="center"
+                  flexDir={{
+                    base: 'column',
+                    md: 'row',
+                  }}
+                >
                   <Image
-                    h="284px"
-                    w="284px"
+                    h={{
+                      base: '284px',
+                      md: '284px',
+                    }}
+                    objectPosition="center"
+                    w={{
+                      base: '100%',
+                      md: '284px',
+                    }}
                     alt={`Upcoming Product ${idx} cover`}
                     src={getImageUrl(product?.cover.data?.attributes?.url)}
                   />
                   <Box>
                     <Flex gap="10px" alignItems="center">
                       <Image src={getImageUrl(product?.icon.data?.attributes?.url)} />
-                      <Text fontSize="20px" lineHeight="32px" fontWeight={600}>
+                      <Text
+                        fontSize={{
+                          base: '18px',
+                          md: '20px',
+                        }}
+                        lineHeight="32px"
+                        fontWeight={600}
+                      >
                         {product?.name}
                       </Text>
                     </Flex>
-                    <Text mt="15px" fontSize="16px" lineHeight="25px" fontWeight={400} mb="45px">
+                    <Text
+                      mt="15px"
+                      fontSize={{
+                        base: '14px',
+                        md: '16px',
+                      }}
+                      lineHeight={{
+                        base: '20px',
+                        md: '25px',
+                      }}
+                      fontWeight={400}
+                      mb="45px"
+                    >
                       {product?.description}
                     </Text>
                     <Link href={product?.website!} passHref>
