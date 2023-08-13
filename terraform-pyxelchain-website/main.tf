49d9b50dcb3d55104223c0a38cbfab439b08b6ac
raypyxis <102776418+raypyxis@users.noreply.github.com>
2023-08-13 09:59:44 +0800

    removing profile coz failing on digger yaml
    removing profile coz failing on digger yaml

--- terraform-pyxelchain-website/main.tf
+++ terraform-pyxelchain-website/main.tf
@@ -17,7 +17,7 @@ terraform {
 
 
 provider "aws" {
-  profile = "default"
+  #profile = "default"
   region = "us-east-2"
 }
 
@@ -28,11 +28,11 @@ resource "aws_key_pair" "operator" {
 
 resource "aws_security_group" "allow_ssh" {
   name = "allow-ssh-sg"
-  
+
   ingress {
-    from_port = "22"
-    to_port = "22"
-    protocol = "tcp"
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -40,15 +40,15 @@ resource "aws_security_group" "allow_ssh" {
 resource "aws_security_group" "allow_outbound" {
   name = "allow-outbound-sg"
   egress {
-    from_port = "0"
-    to_port = "0"
-    protocol = "-1"
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "pyxelchain-website2" {
-  ami           = "${var.instance_ami}"
+  ami           = var.instance_ami
   instance_type = "t3.medium"
 
   tags = {
@@ -58,7 +58,7 @@ resource "aws_instance" "pyxelchain-website2" {
 
   key_name = "operator-key"
 
-  subnet_id = "${aws_subnet.public_a.id}"
+  subnet_id = aws_subnet.public_a.id
 
   vpc_security_group_ids = [
     "${aws_security_group.ssh.id}",
@@ -70,7 +70,7 @@ resource "aws_instance" "pyxelchain-website2" {
   ebs_block_device {
     device_name           = "/dev/sdg"
     volume_type           = "gp2"
-    volume_size           = 80 
+    volume_size           = 80
     delete_on_termination = true
     # snapshop_id           = ""
   }
@@ -78,7 +78,7 @@ resource "aws_instance" "pyxelchain-website2" {
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = "${file("./id_ed25519")}"
+    private_key = file("./id_ed25519")
     host        = aws_instance.pyxelchain-website2.public_ip
     timeout     = "3m"
   }
@@ -114,14 +114,14 @@ resource "aws_instance" "pyxelchain-website2" {
 }
 
 resource "aws_eip" "instance_ip" {
-  instance = "${aws_instance.pyxelchain-website2.id}"
+  instance = aws_instance.pyxelchain-website2.id
 
   tags = {
-    env  = "pyxis-website-${var.env}"
+    env = "pyxis-website-${var.env}"
   }
 }
 
 output "instance_ip" {
-  value = "${aws_eip.instance_ip.public_ip}"
+  value = aws_eip.instance_ip.public_ip
 }
 
