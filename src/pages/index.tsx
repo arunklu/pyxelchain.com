ffe2dc4290136dbaa254449f1fe84625975c22e4
Ralph Largo <devlargs@gmail.com>
2023-10-05 18:24:32 +0800

    feat: DEV-128: converted index page to static fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,55 +1,62 @@
-import React from 'react'
-import axios from 'axios'
-import { print } from 'graphql'
+import { useQuery } from '@apollo/client'
 import { Box } from '@chakra-ui/react'
-import { GetServerSideProps } from 'next'
-
 import { SEO } from '@components/seo'
-import Landing from '@modules/home/landing'
+import Spinner from '@components/spinner'
+import { StrapiContextProvider } from '@context/strapi-context'
+import { HOME_QUERY } from '@graphql/queries/home'
 import About from '@modules/home/about/about'
-import Vision from '@modules/home/vision-problem-solution'
-import Team from '@modules/home/team'
-import Roadmap from '@modules/home/roadmap'
+import Landing from '@modules/home/landing'
 import Partnership from '@modules/home/partnership'
 import Pyxis from '@modules/home/pyxis/pyxis'
-import { StrapiContextProvider } from '@context/strapi-context'
-
-import { CopyEntity, FeatureEntity, TeamEntity, UpcomingProductEntity } from 'types/index'
-import { HOME_QUERY } from '@graphql/queries/home'
+import Roadmap from '@modules/home/roadmap'
+import Team from '@modules/home/team'
 import UpcomingProducts from '@modules/home/upcoming-products'
+import Vision from '@modules/home/vision-problem-solution'
+import { isBrowser } from 'framer-motion'
+import React from 'react'
+import {
+  Copy,
+  CopyEntity,
+  Feature,
+  FeatureEntity,
+  TeamEntity,
+  Team as TeamProps,
+  UpcomingProductEntity,
+} from 'types/index'
 
-interface PageProps {
-  data: {
-    allStrapiCopy: {
-      nodes: NonNullable<CopyEntity['attributes']>[]
-    }
-    allStrapiFeature: {
-      nodes: NonNullable<FeatureEntity['attributes']>[]
-    }
-    allStrapiTeam: {
-      nodes: NonNullable<TeamEntity['attributes']>[]
-    }
-    upcomingProduct: {
-      data: UpcomingProductEntity
-    }
+interface HomeQuery {
+  allStrapiCopy: {
+    data: CopyEntity[]
+  }
+  allStrapiFeature: {
+    data: FeatureEntity[]
+  }
+  allStrapiTeam: {
+    data: TeamEntity[]
+  }
+  upcomingProduct: {
+    data: UpcomingProductEntity
   }
 }
 
-const Index: React.FC<PageProps> = ({ data }) => {
-  const values = {
-    copies: data.allStrapiCopy.nodes,
-    features: data.allStrapiFeature.nodes,
-    teams: data.allStrapiTeam.nodes,
-    upcomingProducts: data.upcomingProduct.data,
+const Index: React.FC = () => {
+  const { data, loading } = useQuery<HomeQuery>(HOME_QUERY)
+
+  if (loading) {
+    return <Spinner />
   }
 
-  return (
+  const values = {
+    features: data?.allStrapiFeature.data.map((c) => c.attributes) as Feature[],
+    copies: data?.allStrapiCopy.data.map((c) => c.attributes) as Copy[],
+    teams: data?.allStrapiTeam.data.map((c) => c.attributes) as TeamProps[],
+    upcomingProducts: data?.upcomingProduct.data as UpcomingProductEntity,
+  }
+
+  return isBrowser ? (
     <StrapiContextProvider values={values}>
       <Box mb={36}>
-        <SEO
-          title={data?.allStrapiCopy?.nodes[0]?.seo?.metatitle}
-          description={data?.allStrapiCopy?.nodes[0]?.seo?.metadescription}
-        />
+        <SEO title={values.copies[0]?.seo?.metatitle} description={values.copies[0].seo?.metadescription} />
         <Landing />
         <UpcomingProducts />
         <About />
@@ -60,59 +67,9 @@ const Index: React.FC<PageProps> = ({ data }) => {
         <Partnership />
       </Box>
     </StrapiContextProvider>
+  ) : (
+    <Spinner />
   )
 }
 
 export default Index
-
-interface QueryResult {
-  data: {
-    allStrapiCopy: {
-      data: CopyEntity[]
-    }
-    allStrapiFeature: {
-      data: FeatureEntity[]
-    }
-    allStrapiTeam: {
-      data: TeamEntity[]
-    }
-    upcomingProduct: {
-      data: UpcomingProductEntity
-    }
-  }
-}
-
-export const getServerSideProps: GetServerSideProps<PageProps> = async () => {
-  const result = await axios.post<QueryResult>(process.env.NEXT_PUBLIC_API_URL as string, {
-    query: print(HOME_QUERY),
-  })
-
-  const copyNodes = result.data.data.allStrapiCopy.data.map(
-    (c) => c.attributes as NonNullable<CopyEntity['attributes']>
-  )
-  const featureNodes = result.data.data.allStrapiFeature.data.map(
-    (c) => c.attributes as NonNullable<FeatureEntity['attributes']>
-  )
-  const teamNodes = result.data.data.allStrapiTeam.data.map(
-    (c) => c.attributes as NonNullable<TeamEntity['attributes']>
-  )
-
-  const upcomingProduct = result.data.data.upcomingProduct
-
-  return {
-    props: {
-      data: {
-        allStrapiCopy: {
-          nodes: copyNodes,
-        },
-        allStrapiFeature: {
-          nodes: featureNodes,
-        },
-        allStrapiTeam: {
-          nodes: teamNodes,
-        },
-        upcomingProduct,
-      },
-    },
-  }
-}
