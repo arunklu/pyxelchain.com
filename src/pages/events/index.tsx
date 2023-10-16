97aeb3baffe9afed5f47cacfb20b5817f59eb7bd
Ralph Largo <devlargs@gmail.com>
2023-10-16 19:52:07 +0800

    feat: WEB-454: completed landing page of pyxelchain events

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,8 +1,10 @@
 import { useQuery } from '@apollo/client'
+import { SEO } from '@components/seo'
 import Spinner from '@components/spinner'
 import { EVENTS_QUERY } from '@graphql/queries/events'
+import Landing from '@modules/events/landing'
 import { FC } from 'react'
-import { EventEntity } from 'types'
+import { Copy, EventEntity } from 'types'
 
 interface EventsQuery {
   allStrapiCopy: {
@@ -11,11 +13,21 @@ interface EventsQuery {
 }
 
 const Events: FC = () => {
-  const { loading } = useQuery<EventsQuery>(EVENTS_QUERY)
+  const { loading, data } = useQuery<EventsQuery>(EVENTS_QUERY)
+  const eventsData = (data?.allStrapiCopy.data.map((c) => c.attributes)?.[0] ?? {}) as Copy
 
   if (loading) return <Spinner />
 
-  return <></>
+  return (
+    <>
+      <SEO title={eventsData.seo?.metatitle} description={eventsData?.seo?.metadescription} />
+      <Landing
+        title={`${eventsData?.title}`}
+        titleWithGradient={`${eventsData?.titleWithGradient}`}
+        description={`${eventsData.description}`}
+      />
+    </>
+  )
 }
 
 export default Events
