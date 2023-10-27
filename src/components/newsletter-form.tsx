62956e228137979517a9beaaeea2148c735e610c
Ralph Largo <devlargs@gmail.com>
2023-10-27 18:26:28 +0800

    fix: WEB-464: newsletter elements are not center aligned

--- src/components/newsletter-form.tsx
+++ src/components/newsletter-form.tsx
@@ -8,19 +8,12 @@ type NewsletterFormTypes = {
 }
 const NewsletterForm: React.FC<NewsletterFormTypes> = ({ autoAlign }) => (
   <>
-    <Box
-      mt={autoAlign ? '48px' : 0}
-      justifyContent="space-between"
-      mx={{ base: 'auto', lg: autoAlign ? 'auto' : 0 }}
-      maxW="600px"
-      p={2}
-      rounded="lg"
-    >
+    <Box mt={autoAlign ? '35px' : 0} justifyContent="space-between" rounded="lg" display="grid" placeContent="center">
       <Link href="/services" mb={8}>
         <Button>Contact Our Team</Button>
       </Link>
     </Box>
-    <Text color="white" mt="35px">
+    <Text color="white" textAlign="center">
       We don't spam, sell, or shill!
     </Text>
   </>
